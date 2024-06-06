c8726e67d5a1c5a6ddfdb25d403fe7dbe54e9b50
pinhong930808 <162553607+pinhong930808@users.noreply.github.com>
2024-06-06 18:02:54 +0800

    0606

---
+++ 0606/ExpilicitlntentDemo2/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ExpilicitlntentDemo2"
+include(":app")
+ 
